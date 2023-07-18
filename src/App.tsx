96bed696c9341426aef0a08413330b3f11fedef2
Georgi Iliev <georgi.iliev533@outlook.com>
2023-07-18 23:37:48 +0300

    minor changes to web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const position = {lat: 51.505, lng: -0.09}
 
 function App() {
-        
-return(
-  <MapContainer className='map' center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-);
-    
+
+    return(
+        <MapContainer className='map' center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        <Marker position={position}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+        </MapContainer>
+    );
+
 }
 
 export default App;
